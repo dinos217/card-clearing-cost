74f5f7299b751c7f18c29dd3ca868492ff98fe01
konkan <konstantinos.kanakakis@axiomatics.com>
2024-10-18 19:16:53 +0300

    Initial commit

---
+++ src/main/java/com/utils/card_clearing_cost/CardClearingCostApplication.java
@@ -0,0 +1,13 @@
+package com.utils.card_clearing_cost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardClearingCostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardClearingCostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utils/card_clearing_cost/CardClearingCostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utils.card_clearing_cost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardClearingCostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
