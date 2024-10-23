8d627214fd637d3ee3cc7146fea23d3adaa9f752
konkan <konstantinos.kanakakis@axiomatics.com>
2024-10-23 22:21:43 +0300

    added api versioning

--- src/main/java/com/utils/card_clearing_cost/controllers/CostCalculationController.java
+++ src/main/java/com/utils/card_clearing_cost/controllers/CostCalculationController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/card-cost")
+@RequestMapping(value = "/v1/card-cost")
 public class CostCalculationController {
 
     private final CostCalculationService costCalculationService;
